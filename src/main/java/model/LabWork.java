09f623942545add9fd6706c99f96facb6d17ccd2
matviikk <matvey.zdor@gmail.com>
2024-03-12 22:41:07 +0300

    commands+

--- src/main/java/model/LabWork.java
+++ src/main/java/model/LabWork.java
@@ -1,6 +1,14 @@
 package model;
 
-public class LabWork {
+import java.time.LocalDate;
+
+public class LabWork implements Comparable<LabWork>{
+    private static Integer generatedId = 0;
+
+    public LabWork() {
+        this.id = generatedId++;
+    }
+
     private Integer id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
     private Coordinates coordinates; //Поле не может быть null
@@ -9,4 +17,88 @@ public class LabWork {
     private int averagePoint; //Значение поля должно быть больше 0
     private Difficulty difficulty; //Поле может быть null
     private Person author; //Поле может быть null
+
+    @Override
+    public String toString() {
+        return "LabWork{" +
+                "id=" + id +
+                '}';
+    }
+
+    public static Integer getGeneratedId() {
+        return generatedId;
+    }
+
+    public static void setGeneratedId(Integer generatedId) {
+        LabWork.generatedId = generatedId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Double getMinimalPoint() {
+        return minimalPoint;
+    }
+
+    public void setMinimalPoint(Double minimalPoint) {
+        this.minimalPoint = minimalPoint;
+    }
+
+    public int getAveragePoint() {
+        return averagePoint;
+    }
+
+    public void setAveragePoint(int averagePoint) {
+        this.averagePoint = averagePoint;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Person getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Person author) {
+        this.author = author;
+    }
+
+    @Override
+    public int compareTo(LabWork o) {
+        return Integer.compare(this.id, o.id);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,77 @@ package org.example;
 
 import model.LabWork;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.TreeSet;
+
 public class Main {
+    public static TreeSet<LabWork> treeSet = new TreeSet<>();
     public static void main(String[] args) {
         LabWork labwork = new LabWork();
-        System.out.println(labwork);
+        LabWork labwork1 = new LabWork();
+        LabWork labwork2 = new LabWork();
+        LabWork labwork3 = new LabWork();
+
+        treeSet.add(labwork);
+        treeSet.add(labwork3);
+        treeSet.add(labwork2);
+        treeSet.add(labwork1);
+        update(1, labwork1);
+
+        System.out.println(treeSet);
+    }
+
+    public static void show(){
+        System.out.println(treeSet);
+    }
+    public static void add(LabWork labWork){
+        treeSet.add(labWork);
+    }
+    public static void update(Integer id, LabWork labWork){
+        LabWork temp = null;
+        for (LabWork lb: treeSet){
+            if (Objects.equals(lb.getId(), id)){
+                temp = lb;
+            }
+        }
+        treeSet.remove(temp);
+        labWork.setId(id);
+        treeSet.add(labWork);
+    }
+    public static void removeById(Integer id){
+        LabWork temp = null;
+        for (LabWork lb: treeSet){
+            if (Objects.equals(lb.getId(), id)){
+                temp = lb;
+            }
+        }
+        treeSet.remove(temp);
+    }
+    public static void clear(){
+        treeSet = new TreeSet<>();
+    }
+    public static void removeGreater(LabWork labWork){
+        List<LabWork> temp = new ArrayList<>();
+        for (LabWork lb: treeSet){
+            if (labWork.compareTo(lb) > 0){
+                temp.add(lb);
+            }
+        }
+        for (LabWork lb: temp){
+            treeSet.remove(lb);
+        }
+    }
+    public static void removeLower(LabWork labWork){
+        List<LabWork> temp = new ArrayList<>();
+        for (LabWork lb: treeSet){
+            if (labWork.compareTo(lb) < 0){
+                temp.add(lb);
+            }
+        }
+        for (LabWork lb: temp){
+            treeSet.remove(lb);
+        }
     }
 }
