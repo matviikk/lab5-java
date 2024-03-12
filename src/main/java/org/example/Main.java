e8b15c031d829510f8a6f82fd0abb23c4328371d
matviikk <matvey.zdor@gmail.com>
2024-03-12 21:27:39 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import model.LabWork;
+
+public class Main {
+    public static void main(String[] args) {
+        LabWork labwork = new LabWork();
+        System.out.println(labwork);
+    }
+}
